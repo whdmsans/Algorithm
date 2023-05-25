979e7a1bd4aede07e3aacd7cb3106684fef04072
whdmsans <103178149+whdmsans@users.noreply.github.com>
2023-05-25 15:56:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string a;
+    cin >> a;
+    cout << a.length();
+}

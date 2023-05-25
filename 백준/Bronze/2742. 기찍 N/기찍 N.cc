5bd0d3278828164e1f052bbd8224581994b8abba
whdmsans <103178149+whdmsans@users.noreply.github.com>
2023-05-25 15:55:58 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+    for (; N > 0; N--) {
+        cout << N << "\n";
+    }
+}

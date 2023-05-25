4b2ebde3ca3ae89b0dc2cb37c8409efd417cdf11
whdmsans <103178149+whdmsans@users.noreply.github.com>
2023-05-25 15:55:15 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N = 0;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cout << i << "\n";
+    }
+}

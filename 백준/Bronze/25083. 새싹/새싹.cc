7018f727333d06b865acb1aeb41d6ec0a50c5d58
whdmsans <103178149+whdmsans@users.noreply.github.com>
2023-06-03 16:25:48 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "         ,r'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |";
+}

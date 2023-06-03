798017089e304ab2270fa809bc27944339e8eb18
whdmsans <103178149+whdmsans@users.noreply.github.com>
2023-06-03 16:16:33 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|";
+}

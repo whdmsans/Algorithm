eefe50bcc525745a7c43f5aa8b4bcc7ebf1ee58f
whdmsans <103178149+whdmsans@users.noreply.github.com>
2023-08-10 16:16:18 +0900

    [Gold V] Title: 다각형의 면적, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+pair<double, double> point[10000];
+
+double CCW(pair<double, double> p1, pair<double, double> p2, pair<double, double> p3) {
+    double res;
+    res = (p2.first - p1.first) * (p3.second - p1.second) - (p3.first - p1.first) * (p2.second - p1.second);
+    return res / 2;
+}
+
+// 13172번
+int main(void) {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    double x, y;
+    double result = 0;
+    cin >> N;
+    cin >> x >> y;
+    point[0] = make_pair(x, y);
+    cin >> x >> y;
+    point[1] = make_pair(x, y);
+    for (int i = 2; i < N; i++) {
+        cin >> x >> y;
+        point[i] = make_pair(x, y);
+        result += CCW(point[0], point[i - 1], point[i]);
+    }
+    result = abs(result);
+    cout << fixed;
+    cout.precision(1);
+    cout << result;
+
+    return 0;
+}
